d2d93aa9f08839e99c473e6d7364e2b93cd90a09
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-11-08 14:01:57 +0800

    extract forkdb.ts and forkdb.spec.ts from blockchain project
    modified to be portable

---
+++ src/forkdb.spec.ts
@@ -0,0 +1,81 @@
+import { expect } from 'chai';
+import 'mocha';
+import { newEnv } from 'typestub-node-lmdb';
+import { openForkDB } from './forkdb';
+
+// tslint:disable:no-unused-expression
+
+describe('forkdb TestSuit', () => {
+  const env = newEnv().open({
+    path: 'data',
+    maxDbs: 10,
+  });
+  const { clearAll, loadRoot } = openForkDB(env);
+
+  it('should load root without error', () => {
+    loadRoot();
+  });
+
+  it('should fork without error', () => {
+    const root = loadRoot();
+    const child = root.fork(1);
+    console.log('forked into:', child.forkId);
+  });
+
+  it('should be able to store and get back value', () => {
+    const root = loadRoot();
+    const txn = root.beginTxn();
+    const key = 'num';
+    const oldValue = txn.getNumber(key) || 0;
+    const newValue = oldValue + 1;
+    txn.putNumber(key, newValue);
+    expect(txn.getNumber(key)).equals(newValue);
+    txn.commit();
+  });
+
+  it('should allow child get value from parent if not updated', () => {
+    const root = loadRoot();
+    let txn = root.beginTxn();
+    txn.putString('foo', 'bar');
+    txn.commit();
+
+    const child = root.fork(1);
+    txn = child.beginTxn();
+    expect(txn.getString('foo')).equals('bar');
+    txn.commit();
+  });
+
+  it('should allow child get value from itself if updated', () => {
+    const root = loadRoot();
+    let txn = root.beginTxn();
+    txn.putString('foo', 'bar');
+    txn.commit();
+
+    const child = root.fork(1);
+    txn = child.beginTxn();
+    txn.putString('foo', 'bar2');
+    expect(txn.getString('foo')).equals('bar2');
+    txn.commit();
+  });
+
+  it('should protect parent state from modification by child', () => {
+    const root = loadRoot();
+    let txn = root.beginTxn();
+    txn.putString('foo', 'bar');
+    txn.commit();
+
+    const child = root.fork(1);
+    txn = child.beginTxn();
+    txn.putString('foo', 'bar2');
+    txn.commit();
+
+    txn = root.beginTxn();
+    expect(txn.getString('foo')).equals('bar');
+    txn.commit();
+  });
+
+  it('should clear all forks without error', () => {
+    clearAll();
+  });
+});
+// tslint:enable:no-unused-expression
---
+++ src/forkdb.ts
@@ -0,0 +1,243 @@
+import {
+  ExtendedTxn,
+  Key,
+  KeyType,
+  newCursor,
+  OpenedEnv,
+} from 'typestub-node-lmdb';
+
+type Fork = {
+  parentID: number;
+  childIDs: number[];
+};
+
+const forkIdToKey = (forkId: number) => 'fork:' + forkId;
+
+export function openForkDB(env: OpenedEnv) {
+  const forkDB = env.openDbi({
+    name: 'fork',
+    create: true,
+  });
+
+  function loadForkRecord(txn: ExtendedTxn, forkId: number): Fork | null {
+    const key = forkIdToKey(forkId);
+    return txn.getObject<Fork>(forkDB, key);
+  }
+
+  function saveForkRecord(txn: ExtendedTxn, forkId: number, fork: Fork) {
+    const key = forkIdToKey(forkId);
+    txn.putObject(forkDB, key, fork);
+  }
+
+  function fork(parentID: number, childID?: number) {
+    const txn = env.beginTxn();
+    // allocate child ID
+    childID = childID || parentID + 1;
+    let child: Fork | null;
+    for (
+      child = loadForkRecord(txn, childID);
+      child !== null;
+      childID = (childID + 1) % (65536 - 10),
+        child = loadForkRecord(txn, childID)
+    ) {
+      // try until this childID is not occupied
+      if (childID === parentID) {
+        console.error(`fork pool is full:`, { parentID, childID });
+        throw new Error('fork pool is full');
+      }
+    }
+    child = {
+      parentID,
+      childIDs: [],
+    };
+    const parent = loadForkRecord(txn, parentID);
+    if (parent === null) {
+      throw new Error(`fork parent '${parentID}' not found`);
+    }
+    parent.childIDs.push(childID);
+    saveForkRecord(txn, childID, child);
+    saveForkRecord(txn, parentID, parent);
+    txn.commit();
+    const dbi = env.openDbi({
+      name: forkIdToKey(childID),
+      create: true,
+    });
+    dbi.close();
+    return childID;
+  }
+
+  function loadParent(childId: number) {
+    if (childId === 0) {
+      // this is root fork, don't have parent
+      return;
+    }
+    const txn = env.beginTxn();
+    const child = loadForkRecord(txn, childId);
+    if (child === null) {
+      txn.abort();
+      throw new Error(`fork record '${childId}' not found`);
+    }
+    const parentId = child.parentID;
+    txn.commit();
+    return loadFork(parentId);
+  }
+
+  function loadFork(forkId: number) {
+    const dbi = env.openDbi({
+      name: forkIdToKey(forkId),
+    });
+    const parent = loadParent(forkId);
+    return {
+      forkId,
+      dbi,
+      fork(childId?: number) {
+        childId = fork(forkId, childId);
+        return loadFork(childId);
+      },
+      beginTxn() {
+        const txn = env.beginTxn();
+        const self = {
+          getString(key: Key, keyType?: KeyType): string | null {
+            let value = txn.getString(dbi, key, keyType);
+            if (value === null && parent) {
+              value = txn.getString(parent.dbi, key, keyType);
+            }
+            return value;
+          },
+          getBinary(key: Key, keyType?: KeyType): Buffer | null {
+            let value = txn.getBinary(dbi, key, keyType);
+            if (value === null && parent) {
+              value = txn.getBinary(parent.dbi, key, keyType);
+            }
+            return value;
+          },
+          getNumber(key: Key, keyType?: KeyType): number | null {
+            let value = txn.getNumber(dbi, key, keyType);
+            if (value === null && parent) {
+              value = txn.getNumber(parent.dbi, key, keyType);
+            }
+            return value;
+          },
+          getBoolean(key: Key, keyType?: KeyType): boolean | null {
+            let value = txn.getBoolean(dbi, key, keyType);
+            if (value === null && parent) {
+              value = txn.getBoolean(parent.dbi, key, keyType);
+            }
+            return value;
+          },
+          putString(key: Key, value: string, keyType?: KeyType): void {
+            txn.putString(dbi, key, value, keyType);
+          },
+          putBinary(key: Key, value: Buffer, keyType?: KeyType): void {
+            txn.putBinary(dbi, key, value, keyType);
+          },
+          putNumber(key: Key, value: number, keyType?: KeyType): void {
+            txn.putNumber(dbi, key, value, keyType);
+          },
+          putBoolean(key: Key, value: boolean, keyType?: KeyType): void {
+            txn.putBoolean(dbi, key, value, keyType);
+          },
+          del(key: Key, keyType?: KeyType): void {
+            txn.del(dbi, key, keyType);
+          },
+          commit(): void {
+            txn.commit();
+          },
+          abort(): void {
+            txn.abort();
+          },
+          getObject<T>(key: Key, keyType?: KeyType): T | null {
+            const value = self.getString(key, keyType);
+            if (value === null) {
+              return value;
+            }
+            return JSON.parse(value);
+          },
+          putObject(key: Key, value: any, keyType?: KeyType): void {
+            self.putString(key, JSON.stringify(value), keyType);
+          },
+        };
+        return self;
+      },
+      /**
+       * if no child, delete directly;
+       * if only one child, merge into child;
+       * if multiple child, keep the fork, but mark for delete
+       * */
+      drop() {
+        const txn = env.beginTxn();
+        const fork = loadForkRecord(txn, forkId);
+        if (fork === null) {
+          if ('silent') {
+            txn.abort();
+            return;
+          }
+          throw new Error(`fork record '${forkId}' not found`);
+        }
+        // if no child, delete directly;
+        if (fork.childIDs.length === 0) {
+          dbi.drop();
+          txn.commit();
+          return;
+        }
+        // if only one child, merge into child;
+        if (fork.childIDs.length === 1) {
+          // TODO
+        }
+        // if multiple child, keep the fork, but mark for delete
+        // TODO
+        txn.abort();
+      },
+    };
+  }
+
+  function loadRoot(forkId: number = 0) {
+    const dbi = env.openDbi({
+      name: forkIdToKey(forkId),
+      create: true,
+    });
+    dbi.close();
+    const txn = env.beginTxn();
+    // auto create fork record if not exist
+    let fork = loadForkRecord(txn, forkId);
+    if (fork === null) {
+      fork = {
+        parentID: 0,
+        childIDs: [],
+      };
+      saveForkRecord(txn, forkId, fork);
+    }
+    txn.commit();
+    return loadFork(forkId);
+  }
+
+  function clearAll() {
+    let txn = env.beginTxn();
+    const cursor = newCursor(txn, forkDB);
+    const forkKeys: string[] = [];
+    for (let key = cursor.goToFirst(); key !== null; key = cursor.goToNext()) {
+      forkKeys.push(key.toString());
+    }
+    txn.commit();
+    txn = env.beginTxn();
+    for (const forkKey of forkKeys) {
+      txn.del(forkDB, forkKey);
+    }
+    txn.commit();
+    for (const forkKey of forkKeys) {
+      console.log('deleted', forkKey);
+      const dbi = env.openDbi({
+        name: forkKey,
+        create: true,
+      });
+      dbi.drop();
+    }
+  }
+
+  return {
+    fork,
+    loadFork,
+    loadRoot,
+    clearAll,
+  };
+}
